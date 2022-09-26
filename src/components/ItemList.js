7c5e011440cba2bba4bb498504a03f66a2c815b9
Hugo Vila <code.hvila@gmail.com>
2022-09-26 18:47:28 +0200

    Correct use of fetch

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,17 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styles from "./ItemList.module.css";
 
 export default function ItemList() {
   const [items, setItems] = useState([]);
 
   console.log(items);
 
-  fetch("http://localhost:3005/items")
-    .then((response) => response.json())
-    .then((json) => setItems(json));
+  useEffect(() => {
+    fetch("http://localhost:3005/items")
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, []);
 
   return (
     <div>
       <h2>Item List</h2>
+      <ul className={styles.ItemList}>
+        {items.map((item) => (
+          <li key={item.id}>
+            <h3>
+              {item.id}. {item.productName}
+            </h3>
+            <p className={styles.price}>
+              <span>{item.price}</span>
+            </p>
+            <p>{item.productDescription}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

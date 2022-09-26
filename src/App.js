6866693b40fe6cc273d42a472a7045c5224f22bb
Hugo Vila <code.hvila@gmail.com>
2022-09-26 13:59:04 +0200

    Create a component to fetch some data from db

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import ItemList from "./components/ItemList";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ItemList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ItemList.js
@@ -0,0 +1,7 @@
+export default function ItemList() {
+  return (
+    <div>
+      <h2>Item List</h2>
+    </div>
+  );
+}

aa0324b6ebb313d53209175a5761907255f7df42
Hugo Vila <code.hvila@gmail.com>
2022-09-26 14:01:10 +0200

    Basic use of fetch

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,7 @@
 export default function ItemList() {
+  fetch("http://localhost:3005/items")
+    .then((response) => response.json())
+    .then((json) => console.log(json));
   return (
     <div>
       <h2>Item List</h2>
